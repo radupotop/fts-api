59c633f52f221895e78f169f16b6166597b689f0
Radu Potop <radu@wooptoo.com>
2019-03-11 21:57:30 +0000

    Create Virtual SQLite table with FTS5 support

--- build.py
+++ build.py
@@ -15,6 +15,17 @@ def import_db():
                 Products.create(orig_id=row[0], product=row[1], brand=row[2])
 
 
+def create_virt_table():
+    '''
+    Create virtual table with FTS5 search.
+    '''
+    db.connect()
+    # db.execute_sql('DROP TABLE search_products')
+    db.execute_sql('CREATE VIRTUAL TABLE search_products USING FTS5(product, brand)')
+    db.execute_sql('INSERT INTO search_products SELECT product, brand FROM products')
+
+
 if __name__ == '__main__':
     build_db()
     import_db()
+    create_virt_table()
